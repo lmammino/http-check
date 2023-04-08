01234147d591a9c0439e15ca25bd24313a325d60
Luciano Mammino <lucianomammino@gmail.com>
2023-04-08 20:25:38 +0200

    Added lambda powertools logs

--- src/app.js
+++ src/app.js
@@ -1,11 +1,24 @@
+import { Logger } from '@aws-lambda-powertools/logger'
+
+const logger = new Logger({ serviceName: 'httpCheck' })
+
 export const handler = async (event, _context) => {
-  // ... get data from event
+  logger.debug('Received event', { event })
+
   const { url } = event
+  if (!url) {
+    logger.error('Missing "url" in event')
+    throw new Error('Missing "url" in event', { event })
+  }
 
-  // ... run business logic
-  const res = await fetch(url)
-  console.info(`Status of ${url} : ${res.status}`)
+  logger.debug('Checking url', { url })
 
-  // ... return a result
-  return res.status
+  try {
+    const res = await fetch(url)
+    logger.info('Url status', { url, status: res.status })
+    return res.status
+  } catch (err) {
+    logger.error('Error checking url', { url, err })
+    throw err
+  }
 }

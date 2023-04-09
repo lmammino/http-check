7510bee65f69fe7d4b1ff37cd3a274299bdf7b6e
Luciano Mammino <lucianomammino@gmail.com>
2023-04-09 12:28:02 +0200

    Added metrics

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import { Logger } from '@aws-lambda-powertools/logger'
+import { Metrics, MetricUnits } from '@aws-lambda-powertools/metrics'
 
 const logger = new Logger({ serviceName: 'httpCheck' })
+const metrics = new Metrics({ namespace: 'utils', serviceName: 'httpCheck' })
 
 export const handler = async (event, _context) => {
   logger.debug('Received event', { event })
@@ -16,9 +18,16 @@ export const handler = async (event, _context) => {
   try {
     const res = await fetch(url)
     logger.info('Url status', { url, status: res.status })
+    const successMetric = metrics.singleMetric()
+    successMetric.addDimension('status', String(res.status))
+    successMetric.addMetric('successfulRequests', MetricUnits.Count, 1)
+    metrics.addMetric(`successfulRequests${String(res.status)[0]}xx`, MetricUnits.Count, 1)
+    metrics.publishStoredMetrics()
     return res.status
   } catch (err) {
     logger.error('Error checking url', { url, err })
+    metrics.addMetric('FailedRequests', MetricUnits.Count, 1)
+    metrics.publishStoredMetrics()
     throw err
   }
 }

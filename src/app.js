696590e3620ae55bd219f8bda9ee6acfcd094d69
Luciano Mammino <lucianomammino@gmail.com>
2023-04-10 15:11:22 +0200

    Added middy

--- src/app.js
+++ src/app.js
@@ -1,12 +1,11 @@
-import { Logger } from '@aws-lambda-powertools/logger'
-import { Metrics, MetricUnits } from '@aws-lambda-powertools/metrics'
+import middy from '@middy/core'
+import { Logger, injectLambdaContext } from '@aws-lambda-powertools/logger'
+import { Metrics, MetricUnits, logMetrics } from '@aws-lambda-powertools/metrics'
 
 const logger = new Logger({ serviceName: 'httpCheck' })
 const metrics = new Metrics({ namespace: 'utils', serviceName: 'httpCheck' })
 
-export const handler = async (event, _context) => {
-  logger.debug('Received event', { event })
-
+const lambdaHandler = async (event, _context) => {
   const { url } = event
   if (!url) {
     logger.error('Missing "url" in event')
@@ -22,12 +21,14 @@ export const handler = async (event, _context) => {
     successMetric.addDimension('status', String(res.status))
     successMetric.addMetric('successfulRequests', MetricUnits.Count, 1)
     metrics.addMetric(`successfulRequests${String(res.status)[0]}xx`, MetricUnits.Count, 1)
-    metrics.publishStoredMetrics()
     return res.status
   } catch (err) {
     logger.error('Error checking url', { url, err })
     metrics.addMetric('FailedRequests', MetricUnits.Count, 1)
-    metrics.publishStoredMetrics()
     throw err
   }
 }
+
+export const handler = middy(lambdaHandler)
+  .use(injectLambdaContext(logger))
+  .use(logMetrics(metrics))
